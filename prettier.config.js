5aa6e7286214ca6059c57875f6bc63ae24c8101a
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-05-17 10:08:40 +0200

    Install prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export default {
+	filepath: '/app',
+	endOfLine: 'lf',
+	semi: true,
+	printWidth: 100,
+	singleQuote: true,
+	quoteProps: 'consistent',
+	tabWidth: 4,
+	useTabs: true,
+	singleAttributePerLine: true,
+};
